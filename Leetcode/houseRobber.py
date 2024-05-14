f2d0815ca4d7353071be87bad939da09940bbc1e
1026sumanrawat <71361095+1026sumanrawat@users.noreply.github.com>
2024-05-14 16:21:39 -0700

    Create houseRobber.py

---
+++ Leetcode/houseRobber.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/house-robber/
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def dp(arr, i, memo):
+            if i >= len(arr) :
+                return 0
+            if i in memo:
+                return memo[i]
+            a = arr[i] + dp(arr, i + 2, memo)
+            b = dp(arr, i + 1, memo)
+            memo[i] = max(a,b)
+            return memo[i]
+
+        if len(nums) <= 2:
+            return max(nums)
+        memo = {}
+        x = dp(nums, 0, memo)
+        y = dp(nums, 1, memo)
+        return max(x,y)
