85a1700145e8078b21c18fa82e3294e7eecd7893
1026sumanrawat <71361095+1026sumanrawat@users.noreply.github.com>
2024-10-05 11:49:03 -0700

    adding 2nd solution longestPalindrome.py

--- Leetcode/longestPalindrome.py
+++ Leetcode/longestPalindrome.py
@@ -29,4 +29,27 @@ class Solution:
                 r += 1
             res.append(sub)
         return max(res, key = len)
-            
+
+# 2nd Solution same approach
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        resLen = 0
+        for i in range(len(s)):
+            l, r = i , i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 >= resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+                l -= 1
+                r += 1
+        # for i in range(len(s)):
+            l , r = i, i+1
+            while l>=0 and r < len(s) and s[l] == s[r]:
+                if r - l + 1 >= resLen:
+                    res = s[l:r+1]
+                    resLen = r -l + 1
+                l -= 1
+                r += 1
+        return res
